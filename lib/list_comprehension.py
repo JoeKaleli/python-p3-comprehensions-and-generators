c20f3edec7ff3e2aac3580dedc8c15e8be15a40c
JoeKaleli <kalelimburu9@gmail.com>
2024-03-15 06:09:56 +0300

    Done with lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = []
+    for n in num_list: 
+        if n % 2 == 0:
+         evens.append(n)
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    sentence = []
+    for n in sentence_list:
+       sentence.append(n + '!')
+    return sentence
